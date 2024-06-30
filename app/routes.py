688942160e85301b1610d2eec83b6474e452c58d
brian <bri4n.huang@gmail.com>
2024-06-30 15:16:23 -0700

    setup

--- app/routes.py
+++
@@ -1,9 +0,0 @@
-from app import app
-from flask import render_template
-
-@app.route('/')
-@app.route('/index')
-def index():
-    title = "Meeting Planner"
-    message = "This is meeting-planner"
-    return render_template('index.html', title=title, message=message)
