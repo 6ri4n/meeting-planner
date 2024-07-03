5300785be64a036dbafe4b13959e8b96aaf5b3aa
brian <bri4n.huang@gmail.com>
2024-07-03 15:34:58 -0700

    update db model

--- app/models.py
+++ app/models.py
@@ -2,13 +2,20 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class Event(db.Model):
+    _id = db.Column(db.Integer, primary_key = True, autoincrement = False, nullable = False)
+    event_name = db.Column(db.String(255), nullable = False)
+    created_at = db.Column(db.String(255), nullable = False)
+    available_days = db.Column(db.String(255), nullable = False) # Comma-separated string of dates
+    time_range = db.Column(db.String(255), nullable = False) # Comma-separated string of start and end times (UTC)
 
-class Events(db.Model):
-    user_id = db.Column(db.Integer, primary_key = True)
+    users = db.relationship('User', backref = 'event', lazy = True)
+
+
+class User(db.Model):
+    _id = db.Column(db.Integer, primary_key = True)
+    event_id = db.Column(db.Integer, db.ForeignKey('event._id'), nullable = False)
     username = db.Column(db.String(80), nullable = False)
     password = db.Column(db.String(120), nullable = False)
-
-    event_id = db.Column(db.Integer, nullable = False)
-    event_creator = db.Column(db.Boolean, nullable = False)
-    available_days = db.Column(db.String(255), nullable = False)  # Comma-separated string of dates
-    time_range = db.Column(db.String(255), nullable = False)  # Comma-separated string of start and end times (UTC)
+    available_days = db.Column(db.String(255), default = None) # Comma-separated string of dates
+    available_time = db.Column(db.String(255), default = None) # Comma-separated string of start and end times (UTC)
