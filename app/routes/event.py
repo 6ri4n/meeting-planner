235b26d59aeb2e47b1214892fcc9b938c47e174a
brian <bri4n.huang@gmail.com>
2024-07-27 14:44:30 -0700

    refactor create event page

--- app/routes/event.py
+++ app/routes/event.py
@@ -11,10 +11,39 @@ def event_home():
 
 @event_bp.route("/create")
 def create_event():
-    return render_template("/event/create.html", title = "Create Event", message = "Create Event", timezones = common_timezones)
+    return render_template(
+        "/event/create.html",
+        timezones = common_timezones,
+        times = [
+            "01:00 AM",
+            "02:00 AM",
+            "03:00 AM",
+            "04:00 AM",
+            "05:00 AM",
+            "06:00 AM",
+            "07:00 AM",
+            "08:00 AM",
+            "09:00 AM",
+            "10:00 AM",
+            "11:00 AM",
+            "12:00 PM",
+            "01:00 PM",
+            "02:00 PM",
+            "03:00 PM",
+            "04:00 PM",
+            "05:00 PM",
+            "06:00 PM",
+            "07:00 PM",
+            "08:00 PM",
+            "09:00 PM",
+            "10:00 PM",
+            "11:00 PM",
+            "12:00 AM"
+        ]
+    )
 
 @event_bp.route("/<event_id>")
 def event(event_id):
     event = Event.query.get(event_id)
     if event:
-        return render_template("/event/event.html", availableDays = json.dumps(event.available_days), participants = event.participants)
+        return render_template("/event/event.html", meeting = json.dumps(event.meeting), participants = event.participants)
