68258f9efd36b11ddc68400ebc86df38bce1e70c
brian <bri4n.huang@gmail.com>
2024-08-05 21:36:52 -0700

    update routes

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,6 +1,4 @@
-from .home import home_bp
 from .event import event_bp
 
 def init_app(app):
-    app.register_blueprint(home_bp, url_prefix="/")
-    app.register_blueprint(event_bp, url_prefix="/event")
+    app.register_blueprint(event_bp, url_prefix="/")
