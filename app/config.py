545ccacdabe6727fc1e3aded0b8a815be4a28a2a
brian <bri4n.huang@gmail.com>
2024-09-21 16:19:11 -0700

    setup deployment

--- app/config.py
+++ app/config.py
@@ -3,11 +3,10 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-db_name = os.getenv("DB_NAME")
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{db_name}.db"
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY")
 
